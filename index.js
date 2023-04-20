b307669b6e1fff2616d675a9d9e879375e81e4d7
saurabh <oye.saurabhyadav@gmail.com>
2023-04-20 19:31:04 +0530

    s

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // to access the API endpoint without triggering the CORS error.
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: `${process.env.BASE_URL}`,
   })
 );
 app.use("/images", express.static(path.join(__dirname, "/images")));
